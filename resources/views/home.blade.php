fa9220190dcffaff32cdaeaab15166a2d978ba34
Victor Montoya <montoyavictor086@gmail.com>
2023-05-10 21:33:37 -0500

    pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,8 @@
                     </div>
                     <div class="col-8 text-center">
                         <h2 id="tituloPrincipal">¡Bienvinidos!</h2>
-                        <p>Lo que nececites puedes buscarlo aqui estamos para ayudarte.</p>
-                        <h4>SEIM</h4>
+                        <p class="text-white">Lo que nececites puedes buscarlo aqui estamos para ayudarte.</p>
+                        <h4 class="text-white">SEIM</h4>
                         <div class="row">
                             <div class="col-md-10 col-sm-12 inputBuscar">
                                 <input type="text" placeholder="¿Qúe estás nececitando?">
@@ -22,21 +22,49 @@
                                 </svg>
                             </div>
                             <div class="col-2 text-end">
-                                <a href="#">Ayuda</a>
+                                <a class="text-white" href="#">Ayuda</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-2 text-end">
-                        <a href="$">Registrate</a>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <a class="text-white" href="$">Registrate</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle text-white" viewBox="0 0 16 16">
                             <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                             <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                           </svg>
                     </div>
-                    <div class="col-12">
-                        <p>Categorias disponibles</p>
-                        <div>
-
+                    <div class="col-12 mt-5">
+                        <p class="text-white">Categorias disponibles</p>
+                            <div class="row ">
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Peluqueria</p>
+                                </div>
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Barberia</p>
+                                </div>
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Cuidado facial y corporal</p>
+                                </div>
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Ropa</p>
+                                </div>
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Accesorios</p>
+                                </div>
+                                <div class="col-md-2 pt-2 col-sm-6 categoria ">
+                                    <img src="{{ asset('img/portada.jpg') }}" alt="">
+                                    <p>Articulos de belleza</p>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="col-12 politicas">
+                        <div class="">
+                            <a href="#">Nuestras politicas</a>
                         </div>
                     </div>
                 </div>
